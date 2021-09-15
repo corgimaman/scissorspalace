9f8fdbae46a58945145dba0eebbef622ee45543e
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-09-14 20:41:53 -0500

    added mongoose and Event model

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# sicssorspalace
+# Scissors Palace
+
+https://www.youtube.com/watch?v=sOUNgOx0GcU
+
+pick up here next time
--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -41,22 +42,44 @@ app.use('/graphql', graphqlHTTP({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event.find().then(events => {
+                return events.map( event => {
+                    return { ...event._doc }
+                })
+            }).catch(err => {
+                throw err;
+            });
         },
         createEvent: (args) => {
-            const event = { 
-                _id: Math.random().toString(),
+
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-             }
+                date: new Date(args.eventInput.date)
+            });
 
-             events.push(event);
-             return event;
+            return event.save().then(result => {
+                console.log(result);
+                return {...result._doc};
+            }).catch(err => {
+                console.log(err);
+                throw err;
+            });
+
         }
     },
     graphiql: true
-}))
+}));
 
-app.listen(3000);
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.1mmr8.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+        )
+    .then(()=>{
+        app.listen(3000);
+        console.log('connected!')
+    })
+    .catch(err => {
+        console.log(err)
+    });
